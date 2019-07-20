7bbe4cc65db815bf7e99dff62ad80560c0d21380
Hardeep Singh <hardeep@cazisoft.com>
2019-07-20 13:14:41 -0400

    Setup home screen with Fragments

---
+++ app/src/main/java/cheema/hardeep/sahibdeep/brotherhood/fragments/NowPlayingFragment.java
@@ -0,0 +1,19 @@
+package cheema.hardeep.sahibdeep.brotherhood.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cheema.hardeep.sahibdeep.brotherhood.R;
+
+
+public class NowPlayingFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_now_playing, container, false);
+    }
+}
---
+++ app/src/main/java/cheema/hardeep/sahibdeep/brotherhood/fragments/RecommendedFragment.java
@@ -0,0 +1,22 @@
+package cheema.hardeep.sahibdeep.brotherhood.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cheema.hardeep.sahibdeep.brotherhood.R;
+import cheema.hardeep.sahibdeep.brotherhood.api.MovieApi;
+import cheema.hardeep.sahibdeep.brotherhood.api.MovieApiProvider;
+
+public class RecommendedFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_recommended, container, false);
+        view.findViewById(R.id.textView);
+        return view;
+    }
+
+}

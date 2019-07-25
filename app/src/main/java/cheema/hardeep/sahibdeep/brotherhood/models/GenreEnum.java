d138f9f85bf876fdffdbe500d82dafa03528bba6
Sahibdeep Singh <sahib1797@gmail.com>
2019-07-25 21:40:02 +0530

    enums updated

--- app/src/main/java/cheema/hardeep/sahibdeep/brotherhood/models/GenreEnum.java
+++ app/src/main/java/cheema/hardeep/sahibdeep/brotherhood/models/GenreEnum.java
@@ -4,7 +4,25 @@ import cheema.hardeep.sahibdeep.brotherhood.R;
 
 public enum GenreEnum {
     ACTION("action", R.drawable.icon_action),
-    ADVENTURE("adventure", R.drawable.icon_adventure);
+    ADVENTURE("adventure", R.drawable.icon_adventure),
+    ANIMATION("animation", R.drawable.icon_animation),
+    COMEDY("comedy",R.drawable.icon_comedy),
+    CRIME("crime", R.drawable.icon_crime),
+    DOCUMENTARY("documentary", R.drawable.icon_documentary),
+    DRAMA("drama", R.drawable.icon_drama),
+    FAMILY("family", R.drawable.icon_family),
+    FANTASY("fantasy", R.drawable.icon_fantasy),
+    HISTORY("history", R.drawable.icon_history),
+    HORROR("horror", R.drawable.icon_horror),
+    MUSIC("music", R.drawable.icon_music),
+    MYSTERY("mystery", R.drawable.icon_mystery),
+    ROMANCE("romance", R.drawable.icon_love),
+    SCIENCE_FICTION("science fiction", R.drawable.icon_scifi),
+    TV_MOVIE("tv movie", R.drawable.icon_tv),
+    THRILLER("thriller", R.drawable.icon_thriller),
+    WAR("war", R.drawable.icon_war),
+    WESTERN("western", R.drawable.icon_western);
+
 
     String name;
     int icon;
